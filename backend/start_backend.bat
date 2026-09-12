c42b867dffce290baddc7df1e3feee229c37b26b
zengshuai <1444689015@qq.com>
2026-09-12 19:40:53 +0800

    4

--- backend/start_backend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d E:\bridge_road_detection\detection_web\backend
-start "django-backend" /b D:\anaconda3\envs\torch\python.exe manage.py runserver 0.0.0.0:8000 --noreload
--- frontend/start_frontend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d E:\bridge_road_detection\detection_web\frontend
-start "frontend-vite" /b npm run dev -- --host 127.0.0.1 --port 5173
